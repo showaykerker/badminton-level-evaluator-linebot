122d1b1461af1fb44883f60b91d0274377d80d58
showaykerker <df405102@gmail.com>
2024-09-06 22:58:27 +0800

    feat: Adjust evaluate logic

--- evaluator.py
+++ evaluator.py
@@ -62,61 +62,63 @@ class Evaluator:
         level = self.evaluate()
         return TextSendMessage(text=f"評估完成！您的羽球分級評估為: {level} 級。此結果僅供參考。")
 
-    def evaluate(self):
-        scores = [int(answer) for answer in self.data.values()]
+    def debug(self):
+        if not self.is_completed():
+            return "尚未完成評估"
+        msg = f"User ID: {self.user_id}\n"
+        msg += "="*20 + "\n"
+        for key, value in self.answer.items():
+            msg += f"{value} | {key}\n"
+        msg += "="*20 + "\n"
+        msg += f"評估結果: {self.evaluate()} 級\n"
+        msg += f"score: {self._get_score()[1]}\n\n"
+        return msg
+
+    def _get_score(self):
+        scores = [int(answer) for answer in self.answer.values()]
         total_score = sum(scores)
+        return scores, total_score
+
+    def evaluate(self):
+        scores, total_score = self._get_score()
 
         if scores[0] == 1:  # 如果完全不了解規則和禮儀
             return 1
 
-        if total_score <= 15:
+        if total_score == 10:
             return 1  # 新手階1級
-        elif total_score <= 20:
+        elif total_score <= 15:
             return 2  # 新手階2級
-        elif total_score <= 25:
+        elif total_score <= 20:
             return 3  # 新手階3級
-        elif total_score <= 30:
+        elif total_score <= 34:
             return 4  # 初階1級
-        elif total_score <= 35:
-            return 5  # 初階2級
-        elif total_score <= 40:
+        elif total_score <= 30:
             if scores[5] >= 3 and scores[6] >= 3:  # 檢查輪轉和進攻球路
                 return 6  # 初中階1級
             else:
-                return 5  # 仍為初階2級
-        elif total_score <= 45:
+                return 5  # 初階2級
+        elif total_score <= 35:
             if scores[5] >= 3 and scores[6] >= 3 and scores[7] >= 3:  # 檢查輪轉、進攻球路和防守
                 return 7  # 初中階2級
+            elif all(score >= 3 for score in scores[5:]):  # 檢查後五個問題是否都至少選3
+                return 8  # 中階1級
             else:
                 return 6  # 初中階1級
-        elif total_score <= 50:
-            if all(score >= 3 for score in scores[5:]):  # 檢查後五個問題是否都至少選3
-                return 8  # 中階1級
-            else:
-                return 7  # 初中階2級
-        elif total_score <= 55:
-            if all(score >= 3 for score in scores[5:]) and scores[8] >= 4:  # 戰術運用至少選4
-                return 9  # 中階2級
-            else:
-                return 8  # 中階1級
-        elif total_score <= 60:
-            if all(score >= 4 for score in scores[5:]):  # 後五個問題都至少選4
-                return 10  # 中進階1級
-            else:
-                return 9  # 中階2級
-        elif total_score <= 65:
-            if all(score >= 4 for score in scores[5:]) and scores[9] == 4:  # 步法移動選4
-                return 11  # 中進階2級
-            else:
-                return 10  # 中進階1級
-        elif total_score <= 70:
-            return 12  # 中進階3級
         else:
-            return 13  # 高階或以上
+            if all(score >= 4 for score in scores[5:]):  # 後五個問題都至少選4
+                return 13  # 高階或以上
+            elif all(score >= 3 for score in scores[5:]) and scores[8] >= 4:  # 戰術運用至少選4
+                return 10  # 中進階1級
+            elif all(score >= 3 for score in scores[5:]):
+                return 9  # 中階2級
+            else:
+                return 8  # 中階1級
 
     def __str__(self):
         msg = f"User ID: {self.user_id}\n"
         for index, question in enumerate(self.questions):
+            if not question in self.answer: break
             msg += f"問題 {index + 1}: {question}\n"
             for key, value in self.questions[question].items():
                 msg += f"  {key}: {value}\n"
