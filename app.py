f6c3acf626975ec39717a9d0eed5037f43be5578
showaykerker <df405102@gmail.com>
2024-09-06 21:09:47 +0800

    feat: add more_info option response

--- app.py
+++ app.py
@@ -2,23 +2,12 @@ import os
 
 from flask import Flask, request, abort
 
-from linebot.v3 import (
-    WebhookHandler
-)
-from linebot.v3.exceptions import (
-    InvalidSignatureError
-)
-from linebot.v3.messaging import (
-    Configuration,
-    ApiClient,
-    MessagingApi,
-    ReplyMessageRequest,
-    TextMessage
-)
-from linebot.v3.webhooks import (
-    MessageEvent,
-    TextMessageContent
-)
+from linebot.v3 import WebhookHandler
+from linebot.v3.exceptions import InvalidSignatureError
+from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
+from linebot.v3.webhooks import MessageEvent, TextMessageContent
+
+from messages import more_info
 
 if os.environ.get('ENV') != 'production':
     from dotenv import load_dotenv
@@ -28,6 +17,8 @@ app = Flask(__name__)
 
 configuration = Configuration(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
+
+USER_DATA = {}
 
 
 @app.route("/callback", methods=['POST'])
@@ -51,14 +42,23 @@ def callback():
 
 @handler.add(MessageEvent, message=TextMessageContent)
 def handle_message(event):
+    if event.source.type != "user":
+        return
+    if event.source.userId not in USER_DATA:
+        USER_DATA[event.source.userId] = {}
     with ApiClient(configuration) as api_client:
+
         line_bot_api = MessagingApi(api_client)
-        line_bot_api.reply_message_with_http_info(
-            ReplyMessageRequest(
-                reply_token=event.reply_token,
-                messages=[TextMessage(text=event.message.text)]
-            )
-        )
+        if event.message.text == "更多資訊":
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text=more_info)]))
+        else:
+            line_bot_api.reply_message_with_http_info(
+                ReplyMessageRequest(
+                    reply_token=event.reply_token,
+                    messages=[TextMessage(text="請輸入「更多資訊」以獲取更多資訊")]))
 
 
 if __name__ == "__main__":
---
+++ messages.py
@@ -0,0 +1,6 @@
+more_info = """
+羽球等級判定機器人
+
+版本：v0.1
+Author: Showaykerker
+"""
