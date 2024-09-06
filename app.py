2f349437c6a77e2141b5091b38c252e45b13e6d4
showaykerker <df405102@gmail.com>
2024-09-06 21:22:44 +0800

    feat: Add LineBot API integration and new message handling
    - Integrated LineBot API for messaging
    - Added handling for different message types
    - Introduced a new URL for level table image display

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import os
 
 from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import *
 
-from linebot.v3 import WebhookHandler
-from linebot.v3.exceptions import InvalidSignatureError
-from linebot.v3.messaging import Configuration, ApiClient, MessagingApi, ReplyMessageRequest, TextMessage
-from linebot.v3.webhooks import MessageEvent, TextMessageContent
-
-from messages import more_info
+from messages import more_info, level_table_url
 
 if os.environ.get('ENV') != 'production':
     from dotenv import load_dotenv
@@ -15,51 +13,53 @@ if os.environ.get('ENV') != 'production':
 
 app = Flask(__name__)
 
-configuration = Configuration(os.environ['CHANNEL_ACCESS_TOKEN'])
+line_bot_api = LineBotApi(os.environ['CHANNEL_ACCESS_TOKEN'])
 handler = WebhookHandler(os.environ['CHANNEL_SECRET'])
 
 USER_DATA = {}
 
-
 @app.route("/callback", methods=['POST'])
 def callback():
-    # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
-
-    # get request body as text
     body = request.get_data(as_text=True)
     app.logger.info("Request body: " + body)
-
-    # handle webhook body
     try:
         handler.handle(body, signature)
     except InvalidSignatureError:
-        app.logger.info("Invalid signature. Please check your channel access token/channel secret.")
         abort(400)
-
     return 'OK'
 
-
-@handler.add(MessageEvent, message=TextMessageContent)
+@handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-    app.logger.info(f"Get event: {event}")
-    if event.source.type != "user":
-        return
-    if event.source.user_id not in USER_DATA:
-        USER_DATA[event.source.user_id] = {}
-    with ApiClient(configuration) as api_client:
 
-        line_bot_api = MessagingApi(api_client)
-        if event.message.text == "更多資訊":
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text=more_info)]))
-        else:
-            line_bot_api.reply_message_with_http_info(
-                ReplyMessageRequest(
-                    reply_token=event.reply_token,
-                    messages=[TextMessage(text="請輸入「更多資訊」以獲取更多資訊")]))
+    message = TextSendMessage(text=event.message.text)
+
+    if event.source.type != "user":
+        app.logger.info(f"Source type is not user: {event.source}")
+        return
+
+    try:
+        user_id = event.source.user_id
+        app.logger.info(f"Get user_id from event.source: {user_id}")
+    except:
+        app.logger.error(f"Can not get user_id from event.source: {event.source}")
+        return
+
+    if user_id not in USER_DATA:
+        USER_DATA[user_id] = {}
+    if event.message.text == "開始測試":
+        message = TextSendMessage(text=USER_DATA[user_id])
+    elif event.message.text == "更多資訊":
+        message = TextSendMessage(text=more_info)
+    elif event.message.text == "分級表":
+        # send image "level_table.jpg"
+        message = ImageSendMessage(
+            original_content_url=level_table_url,
+            preview_image_url=level_table_url
+        )
+    else:
+        message = TextSendMessage(text=f"Got message: {event.message.text}")
+    line_bot_api.reply_message(event.reply_token, message)
 
 
 if __name__ == "__main__":
--- messages.py
+++ messages.py
@@ -4,3 +4,5 @@ more_info = """
 版本：v0.1
 Author: Showaykerker
 """
+
+level_table_url = "https://scontent-tpe1-1.xx.fbcdn.net/v/t1.6435-9/198038327_324577535828328_6570501766066799730_n.png?_nc_cat=106&ccb=1-7&_nc_sid=f727a1&_nc_ohc=7XJgFbkmYaYQ7kNvgFhG5By&_nc_ht=scontent-tpe1-1.xx&oh=00_AYAwde3o-2Vo2uJLv6c1N6qk_LEha6hAKPUjQGQjVpfm3Q&oe=67026F24"
